72d8ac520dedc00d4deb6b9bc5976b238eb900a0
mauvais2 <76968305+mauvais2@users.noreply.github.com>
2022-02-02 13:11:16 -0800

    Update test_capitalize.py

--- test_capitalize.py
+++ test_capitalize.py
@@ -6,5 +6,5 @@ def test_capitalize_string():
 def test_capitalize_string_fail():
     assert cap.capitalize_string('test') != 'test'
 
-def test_capitalize_atom_string_fail():
-    assert cap.capitalize_string('atom') != 'Atom'
+def test_capitalize_atom_string():
+    assert cap.capitalize_string('atom') == 'Atom'
