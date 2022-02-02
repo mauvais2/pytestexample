2ccd8e36edf5c4c116cd1c9333d98551b74ad997
mauvais2 <76968305+mauvais2@users.noreply.github.com>
2022-02-02 09:59:20 -0800

    Update test_capitalize.py

--- test_capitalize.py
+++ test_capitalize.py
@@ -1,10 +1,5 @@
 import cap_string as cap
 
-def capitalize_string(s):
-    if not isinstance(s, str):
-        raise TypeError('Please provide a string')
-    return s.capitalize()
-
 def test_capitalize_string():
     assert cap.capitalize_string('test') == 'Test'
     
